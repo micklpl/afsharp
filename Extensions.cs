94c2e969d0c38358eb94ab76a606f753224ce427
mickl <majkelkl@gmail.com>
2014-06-11 21:05:05 +0200

    Aggregate method returns IEnumerable<T>

--- Extensions.cs
+++ Extensions.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
 namespace AFSharp
 {
     public static class Extensions
     {
-        public static AggregateResult Aggregate<T, T2>(this MongoCollection<T> collection, 
+        public static IEnumerable<T2> Aggregate<T, T2>(this MongoCollection<T> collection, 
             Func<AggregationDescriptor<T>, AggregationDescriptor<T2>> pipes)
         {
             var pipeline = pipes.Invoke(new AggregationDescriptor<T>());
             AggregateResult result = collection.Aggregate(pipeline.GetPipeline());
-            return result;
+            return from doc in result.ResultDocuments select BsonSerializer.Deserialize<T2>(doc);
         }
     }
 }
